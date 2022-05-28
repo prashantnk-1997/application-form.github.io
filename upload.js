6d582272c9d13690c79338fb65ad79b41ad588b6
prashantnk-1997 <pacchink8@gmail.com>
2022-05-28 13:32:45 +0530

    my new project

---
+++ upload.js
@@ -0,0 +1,16 @@
+function next2(){
+
+    if(photo.value, sign.value == 0){
+        document.getElementById('error').innerHTML="Please Upload All files"
+      }
+    
+     else{
+        
+        location.assign("file:///D:/mini%20projects/application%20form/submit.html")   
+     }
+
+}
+
+function back(){
+    location.assign("file:///D:/mini%20projects/application%20form/academic.html")
+}
